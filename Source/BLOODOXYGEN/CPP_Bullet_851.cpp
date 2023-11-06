9944fcd4ed26b943c39bc61d389d2845b7b15836
GSDCow <makuradenetaina@gmail.com>
2023-11-07 01:17:22 +0900

    最初のコミット
    弾コンポーネント追加（追加テスト）

---
+++ Source/BLOODOXYGEN/CPP_Bullet_851.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_Bullet_851.h"
+
+// Sets default values
+ACPP_Bullet_851::ACPP_Bullet_851()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPP_Bullet_851::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPP_Bullet_851::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
